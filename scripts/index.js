d745d7384c8e400cd65c71fe8b316b705bf9469c
keirattlexnake <adiyazam03@gmail.com>
2022-06-28 12:03:24 +0700

    tanggal

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jun 28, 2022',
+  birthdate: 'Jun 29, 2022',
   name: 'Fiaa'
 };
 
